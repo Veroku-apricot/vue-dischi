17d35e6faf0a486e19c6234f9fbd9a62f99be6eb
Veronika <veronikakuchtova95@gmail.com>
2020-12-02 09:41:41 +0100

    Better solution

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ var app = new Vue ({
   mounted: function () {
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then(response => {
-        var i = 0;
-        while (i < response.data.response.length) {
-          this.albums.push(response.data.response[i]);
-          i++;
-        }
+        // var i = 0;
+        // while (i < response.data.response.length) {
+        //   this.albums.push(response.data.response[i]);
+        //   i++;
+        // }
+
+        this.albums = response.data.response;
       });
     console.log(this.albums)
   }
-
-
-})
+});
